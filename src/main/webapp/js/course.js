2b481e2176d4c2b5aac484221c9a008ce9a3873c
YannickVandereycken <91282328+YannickVandereycken@users.noreply.github.com>
2022-11-21 11:47:21 +0100

    removed js files

--- src/main/webapp/js/course.js
+++
@@ -1,5 +0,0 @@
-const course = [
-    {"name": "Web3", "lectors":"Greetje Jongen, Elke Steegmans", credits: 3, semester: 1},
-    {"name": "Web4", "lectors":"Johan Pieck, Elke Steegmans", credits: 3, semester: 2},
-    {"name": "Software Testing", "lectors":"Johan Pieck, David Vandenbroeck", credits: 2, semester: 1}
-]
